9f966a0ccdaf9627de7cfa5813ecf2200630b94f
John Mwangi Gikundi <mwasjoh5479@gmail.com>
2025-09-14 01:35:33 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+name=input("what is your name?")
+for i in range(3):
+    print("hello", name)
+    print("Mungu anakupenda sana")
---
+++ mwa.py
@@ -0,0 +1,6 @@
+name=input("Unaitwa nani?")
+for i in range(1):
+    print("habari", name)
+    print("please subscribe to my channel")
+    print("Mungu akubariki")
+    print("God Bless you")
