0d28a1da6b3f56e8af68b620b38b772dfee7da8b
John Mwangi Gikundi <mwasjoh5479@gmail.com>
2025-09-14 20:21:29 +0300

    first commit

---
+++ Menu.py
@@ -0,0 +1,5 @@
+name="menu"
+for i in range(2):
+    print("welcome to Pristine restaurant", name)
+    print("1. Mandazi")
+    print("2. Chips and chicken")
---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+print("Welcome to Pristine Restaurant")
+print("What would you like to order?")
+print("1. Mandazi")
+print("2. Chips and Chicken")
+
+choice = input("Enter the number of your choice (1 or 2): ")
+
+if choice == "1":
+    print("You chose Mandazi. Great choice!")
+elif choice == "2":
+    print("You chose Chips and Chicken. Delicious!")
+else:
+    print("Sorry, that is not on the menu.")
